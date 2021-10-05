538c4eec39f8903093fff3f80839dacb70c1bb6d
divyesh-ram22 <Divyeshram753@gmail.com>
2021-10-05 23:32:46 +0530

    added Play page

--- src/App.jsx
+++ src/App.jsx
@@ -4,9 +4,11 @@ import { Route } from "react-router-dom";
 import DefaultHOC from "./HOC/Default.HOC";
 import MovieHOC from "./HOC/Movie.HOC";
 
+
 //components
 import HomePage from "./pages/Home.page";
 import MoviePage from "./pages/Movie.page";
+import Plays from "./pages/Plays.page";
 
 
 
@@ -22,6 +24,7 @@ function App() {
    <>
      <DefaultHOC path="/" exact component={HomePage} />
      <MovieHOC path="/movie/:id" exact component={MoviePage} />
+     <DefaultHOC path="/plays" exact component={Plays} />
    </>
   );
 }
